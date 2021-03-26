2be77e14896878a6765383e34226fb23dbd9b4da
Clark Tyra <clark.tyra@gmail.com>
2021-03-26 09:15:11 -0700

    got the post route to make posts to the db.json file

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path")
 const app = express()
 const PORT = 3000
 const data = require("./db/db.json")
+const { v4: uuidv4 } = require('uuid');
+const util = require('util');
+const fs = require('fs');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,9 +17,46 @@ app.use(express.static("public"));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(data));
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        res.json(JSON.parse(data))
+    })
+}
+//res.json(data)
+);
 
-// app.post('/api/notes', (req, res) => res.json(data))
+app.post('/api/notes', (req, res) => {
+        console.log("req.body : ", req.body)
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text, 
+            id: uuidv4()
+        }
+        console.log("newNote : ", newNote)
+        //get the dbnotes
+        // parse out array
+        //push to array
+        // stringify it
+        //write to file with new array
+        fs.readFile("./db/db.json", "utf8", function (err, data) {
+            console.log("data: ", data)
+            let parsedNotes;
+
+            parsedNotes = JSON.parse(data)
+            console.log("parsedNotes: ", parsedNotes)
+
+            parsedNotes.push(newNote)
+            console.log("parsedNotesAdded: ", parsedNotes)
+
+            parsedNotes = JSON.stringify(parsedNotes);
+            console.log("stringifiedParsedNotesAdded: ", parsedNotes)
+            
+            // return parsedNotes
+            writeFileAsync('db/db.json', parsedNotes);
+        })
+        
+    }
+)
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
